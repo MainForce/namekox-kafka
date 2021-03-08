9d43918e63d7dab08a0957b6ee98cce3fb100c8b
李满满 <limanman@corp.netease.com>
2021-03-08 16:48:23 +0800

    :underage:

--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ readme = open(os.path.join(curdir, 'READIT.md'), 'rb', 'utf-8').read()
 
 setup(
     name='namekox-kafka',
-    version='0.0.4',
+    version='0.0.5',
     description='namekox kafka',
     long_description_content_type='text/markdown',
     long_description=readme,
     author='forcemain',
     url='https://forcemain.github.io/',
     packages=find_packages(exclude=['test', 'test.*']),
-    install_requires=["namekox-core>=0.0.1", "kafka-python>=2.0.2", "anyjson>=0.3.3"],
+    install_requires=["namekox-core", "kafka-python==2.0.2", "anyjson==0.3.3"],
     zip_safe=True,
     license='Apache License, Version 2.0',
     classifiers=[
